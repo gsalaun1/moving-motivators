3a3a3bdd21c602a4c8c50d888df5374b4ea75e8a
Gaël Salaün <salaunga@yahoo.fr>
2024-06-22 15:53:30 +0000

    Zoom on top left

---
+++ src/HorizontalInfoBar.tsx
@@ -0,0 +1,29 @@
+import arrowLeft from './assets/arrow-left-solid.svg'
+import arrowRight from './assets/arrow-right-solid.svg'
+import './HorizontalInfoBar.css'
+
+type HorizontalInfoBarProps = {
+    step: number
+}
+
+const HorizontalInfoBar = ({step}: HorizontalInfoBarProps) => {
+    if (step == 1) {
+        return (
+            <div className={"info-bar"}>
+                <div>
+                    <img src={arrowLeft}/>
+                    <span>Moins important</span>
+                </div>
+                <div>
+                    <span>Plus important</span>
+                    <img src={arrowRight}/>
+                </div>
+            </div>
+        )
+    }
+    return (
+        <></>
+    )
+}
+
+export default HorizontalInfoBar;

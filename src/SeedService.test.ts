a1401cbf253b582760f37c53057273fb2257ba85
gsalaun1 <gsalaun1@users.noreply.gitlab.com>
2024-06-22 22:16:55 +0200

    Bug fix

--- src/SeedService.test.ts
+++ src/SeedService.test.ts
@@ -15,112 +15,112 @@ test('should forge seeds', () => {
     const cards =
         [
             {
-                id: 9,
+                id: 10,
                 image: statutCard,
                 y: 130
             },
             {
-                id: 8,
+                id: 9,
                 image: relationsSocialesCard,
                 y: -20
             },
 
             {
-                id: 7,
+                id: 8,
                 image: pouvoirCard,
                 y: 70
             },
             {
-                id: 6,
+                id: 7,
                 image: ordreCard,
                 y: 60
             },
             {
-                id: 5,
+                id: 6,
                 image: maitriseCard,
                 y: 50
             },
             {
-                id: 4,
+                id: 5,
                 image: liberteCard,
                 y: 40
             },
             {
-                id: 3,
+                id: 4,
                 image: honneurCard,
                 y: 30
             },
             {
-                id: 2,
+                id: 3,
                 image: curiositeCard,
                 y: -20
             },
             {
-                id: 1,
+                id: 2,
                 image: butCard,
                 y: 10
             },
             {
-                id: 0,
+                id: 1,
                 image: acceptationCard,
                 y: 0
             },
         ]
     const forgedSeed = seedForger(cards)
-    expect(forgedSeed).toStrictEqual("9!130|8!-20|7!70|6!60|5!50|4!40|3!30|2!-20|1!10|0!0")
+    expect(forgedSeed).toStrictEqual("10!130|9!-20|8!70|7!60|6!50|5!40|4!30|3!-20|2!10|1!0")
 })
 
 test('should extract cards', () => {
-    const extractedCards = seedExtracter("9!90|8!-0.5|7!70|6!60|5!50|4!40|3!30|2!-20|1!10|0!0")
+    const extractedCards = seedExtracter("10!90|9!-0.5|8!70|7!60|6!50|5!40|4!30|3!-20|2!10|1!0")
     expect(extractedCards).toStrictEqual([
         {
-            id: 9,
+            id: 10,
             image: statutCard,
             y: 90
         },
         {
-            id: 8,
+            id: 9,
             image: relationsSocialesCard,
             y: -0.5
         },
 
         {
-            id: 7,
+            id: 8,
             image: pouvoirCard,
             y: 70
         },
         {
-            id: 6,
+            id: 7,
             image: ordreCard,
             y: 60
         },
         {
-            id: 5,
+            id: 6,
             image: maitriseCard,
             y: 50
         },
         {
-            id: 4,
+            id: 5,
             image: liberteCard,
             y: 40
         },
         {
-            id: 3,
+            id: 4,
             image: honneurCard,
             y: 30
         },
         {
-            id: 2,
+            id: 3,
             image: curiositeCard,
             y: -20
         },
         {
-            id: 1,
+            id: 2,
             image: butCard,
             y: 10
         },
         {
-            id: 0,
+            id: 1,
             image: acceptationCard,
             y: 0
         },
